0aee9391612589709ed034c3db37883f45cf5978
Elton Ortiz <elton.ortiz1@gmail.com>
2020-01-05 18:33:18 -0300

    deploy test

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -10,7 +10,7 @@ export default class FetchWeather extends React.Component {
 
   async componentDidMount() {
 
-    const url = "http://api.openweathermap.org/data/2.5/weather?id=" + this.props.countryId + "&appid=92cd0c11cf4b16aeda1776f3537e7d26";
+    const url = "https://api.openweathermap.org/data/2.5/weather?id=" + this.props.countryId + "&appid=92cd0c11cf4b16aeda1776f3537e7d26";
     const response = await fetch(url);
     const data = await response.json();
     this.setState({ temp: data.main.temp })
